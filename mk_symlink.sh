2cbf3f15c9e4f60612d229980e5baa9bfb6fc49e
Ibuki Nakamura <ibuki.nakamura@gmail.com>
2020-07-17 15:19:33 +0900

    feat: remove default lint files

--- mk_symlink.sh
+++ mk_symlink.sh
@@ -6,7 +6,6 @@ cd ${HOME}
 mkdir .config
 mkdir .config/nvim
 mkdir .config/fish
-mkdir .config/yamllint
 
 # create symlink
 
@@ -18,10 +17,5 @@ ln -sfvn $HOME/dotfiles/tmux.conf .tmux.conf
 ln -sfvn $HOME/dotfiles/tmuxinator .tmuxinator
 ln -sfvn $HOME/dotfiles/zshenv .zshenv
 ln -sfvn $HOME/dotfiles/zshrc .zshrc
-ln -sfvn $HOME/dotfiles/rubocop.yml .rubocop.yml
-ln -sfvn $HOME/dotfiles/eslintrc .eslintrc
-ln -sfvn $HOME/dotfiles/pug-lintrc .pug-lintrc
-ln -sfvn $HOME/dotfiles/stylelintrc .stylelintrc
-ln -sfvn $HOME/dotfiles/config/yamllint/config .config/yamllint/config
 ln -sfvn $HOME/dotfiles/config/nvim/init.vim .config/nvim/init.vim
 ln -sfvn $HOME/dotfiles/config/fish/config.fish .config/fish/config.fish
